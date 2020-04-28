318d445115e4379d7357fc40c61976ed06a0ed55
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-04-28 16:52:54 -0400

    seting up routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,15 @@
 const db = require("../model");
 const router = require("express").Router();
+
+//gets all workouts
+router.get("/workouts", (req, res) => {
+});
+//gets the first 7 workouts
+router.get("/workouts/range", (req, res) => {
+});
+//creates a new workout
+router.post("/workouts", (req, res) => {
+});
+//updates a workout
+router.put("/workouts/:id", (req, res) => {
+});
