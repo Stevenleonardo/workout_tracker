a99d33750ea0419e13d6c16a576a9a7147b8cdaf
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-04-29 00:24:53 -0400

    api routes completed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,33 @@
-const db = require("../model");
+const db = require("../models");
 const router = require("express").Router();
 
 //gets all workouts
 router.get("/workouts", (req, res) => {
+    db.Workouts.find({}, (err, data)=>{
+        if (err) throw err;
+        res.json(data);
+    })
 });
 //gets the first 7 workouts
 router.get("/workouts/range", (req, res) => {
+    db.Workouts.find({}, (err, data)=>{
+        if (err) throw err;
+        res.json(data);
+    })
 });
 //creates a new workout
-router.post("/workouts", (req, res) => {
+router.post("/workouts", ({body}, res) => {
+    db.Workouts.create(body, (err, data)=>{
+        if (err) throw err;
+        res.json(data);
+    })
 });
 //updates a workout
 router.put("/workouts/:id", (req, res) => {
+    db.Workouts.findByIdAndUpdate(req.params.id, {$push:{exercises:req.body}}, (err,data)=>{
+        if (err) throw err;
+        res.json(data);
+    })
 });
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,8 +4,7 @@ const path = require("path");
 
 //exercise route page
 
-router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html"))
-);
+
 router.get("/exercise", (req, res) => res.sendFile(path.join(__dirname, "../public/exercise.html"))
 );
 router.get("/stats", (req, res) => res.sendFile(path.join(__dirname, "../public/stats.html"))
